4ce176b2cfb88043c40794d6e289ed61d4cce81b
kitt @ttikitt <yosshi1123@gmail.com>
2018-04-15 01:47:50 +0900

    added cmd refresh
    Signed-off-by: kitt @ttikitt <yosshi1123@gmail.com>

--- src/cmd/refresh.js
+++ src/cmd/refresh.js
@@ -1,12 +1,20 @@
 
 module.exports = (req, res, msg) => {
   global.logger.debug('refresh start');
-console.log(msg);
-  if(msg.params.rIndex == 0){
-    res.end("{}");
-  }else{
-    res.end("{}");
+  let result = {};
+  if(global.stateHolder.roomData[msg.room]){
+    room = global.stateHolder.roomData[msg.room].data;
+    
+    if(msg.params.rIndex == 0){
+      result.isFirstChatRefresh = true;
+      
+      result = Object.assign(result, room);
+    }else{
+    
+    }
   }
+  
+  global.logger.debug('refresh end:', result);
+  res.end(JSON.stringify(result));
 
-  global.logger.debug('refresh end:', {});
 }
